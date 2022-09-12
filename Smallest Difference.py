f70f1e410fbf4b2e4090e12cceb2e61d8ae9d59f
akhanda99 <nashit.1999@gmail.com>
2022-09-13 03:06:21 +0600

    Sept 13,22

---
+++ Smallest Difference.py
@@ -0,0 +1,22 @@
+def smallestDifference(arrayOne, arrayTwo):
+    # Write your code here.
+    difList = []
+    ansList = []
+    for i in range(0, len(arrayOne)):
+        for j in range(0, len(arrayTwo)):
+            dif = abs(arrayOne[i] - arrayTwo[j])
+            difList.append(dif)
+            if min(difList) == difList[-1]:
+                x = arrayOne[i]
+                y = arrayTwo[j]
+
+    ansList.append(x)
+    ansList.append(y)
+    return ansList
+
+
+#Test 01
+arrayOne = [-1, 5, 10, 20, 28, 3]
+arrayTwo = [26, 134, 135, 15, 17]
+ans = smallestDifference(arrayOne, arrayTwo)
+print(ans)
